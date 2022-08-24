b637fce3961f6000a46eef48ed65ed9c76aed2e7
Dan Kodin <dan_kodin03@mail.ru>
2022-08-24 12:47:16 +0300

    Задача 2

---
+++ Семинар_1/Задача_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два целых числа:");
+int max = 0;
+int min = 0;
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 < num2) 
+{
+    min = num1;
+    max = num2;
+}
+else {
+    min = num2;
+    max = num1;
+}
+Console.WriteLine("Меньшее число: " + min);
+Console.Write("Большее число: " + max);
+
+
