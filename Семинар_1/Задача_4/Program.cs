1f143d0e260bc600d99811788de13b359f73544c
Dan Kodin <dan_kodin03@mail.ru>
2022-08-24 13:42:15 +0300

    Добавлено условие задачи

--- Семинар_1/Задача_4/Program.cs
+++ Семинар_1/Задача_4/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите три целых числа:");
+﻿
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три целых числа:");
 int max = 0;
 int num1 = int.Parse(Console.ReadLine());
 int num2 = int.Parse(Console.ReadLine());
