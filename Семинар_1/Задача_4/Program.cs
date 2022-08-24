1f8761d66453d133c1b4966adc3d3cc670144f18
Dan Kodin <dan_kodin03@mail.ru>
2022-08-24 13:06:16 +0300

    Задача 4

---
+++ Семинар_1/Задача_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите три целых числа:");
+int max = 0;
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+if (num1 > num2) max = num1;
+else max = num2;
+if (num3 > max) max = num3;
+Console.Write("Максимум: " + max);
