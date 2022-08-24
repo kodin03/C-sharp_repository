08976d1f988df8f5e6f1bae73d854d2d5ae9a827
Dan Kodin <dan_kodin03@mail.ru>
2022-08-24 13:32:44 +0300

    Задача 8

---
+++ Семинар_1/Задача_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое целое число:");
+int even = 0;
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i < num; i++)
+{
+    if (i % 2 == 0) 
+    {
+        even = i;
+        Console.Write(even + " ");
+    }
+    else continue;
+}
+
