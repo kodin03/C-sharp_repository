b92f6fd9f0c192378bdf1675acc0c9ed87cbb91c
Dan Kodin <dan_kodin03@mail.ru>
2022-08-25 08:51:54 +0300

    Задача_13

---
+++ Семинар_2/Задача_13/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите целое число:");
+int num2 = 0;
+int num3 = 0;
+int num = int.Parse(Console.ReadLine());
+if ((num / 100) > 0) 
+{
+    num2 = num / 100;
+    num3 = num2 % 1000;
+    num2 = num3 % 10;
+    Console.Write(num2);
+}
+else Console.WriteLine("Третьей цифры нет");
