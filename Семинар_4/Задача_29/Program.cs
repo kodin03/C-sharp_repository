5d9e21d9bcd34019bbdd281c06cb86b7d1d5b8ae
Dan Kodin <dan_kodin03@mail.ru>
2022-09-06 10:42:23 +0300

    Задача 29

---
+++ Семинар_4/Задача_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из N элементов, заполненных случайнми числами из [a, b) и выводит их на экран.
+
+Console.WriteLine("Введите количество чисел:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начало диапазона чисел:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конец диапазона чисел:");
+int b = int.Parse(Console.ReadLine());
+int[] num = new int[n];
+Random random = new Random();
+
+
+for (int i = 0; i < n; i++) 
+{
+    int number = random.Next(a, b);
+    num[i] = number;
+    if (i == n-1) Console.Write(num[i]);
+    else Console.Write(num[i] + ", ");
+}
