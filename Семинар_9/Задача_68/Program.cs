1baebdc32b454163ed385aa8f5a1e90290e5a522
Dan Kodin <dan_kodin03@mail.ru>
2022-09-18 14:35:12 +0300

    Добавлена проверка на отрицательное число

--- Семинар_9/Задача_68/Program.cs
+++ Семинар_9/Задача_68/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+
 int A(int m, int n)
 {
     if (m == 0) return n + 1;
@@ -14,4 +15,5 @@ Console.Write("Введите число m: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите число n: ");
 int n = int.Parse(Console.ReadLine());
-Console.Write(A(m, n));
+if (m >= 0 && n >= 0) Console.Write(A(m, n));
+else Console.Write("Введите неотрицательное число");
