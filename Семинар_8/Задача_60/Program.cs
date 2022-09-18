04d6e2ed2f3ba75fd1810b72adac88dcbed642f2
Dan Kodin <dan_kodin03@mail.ru>
2022-09-18 11:21:30 +0300

    Задача 60

---
+++ Семинар_8/Задача_60/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце. 
+
+int[,,] array = new int[2, 2, 2];
+
+void Result(int[,,] array) {
+    
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            for (int k = 0; k < array.GetLength(2); k++) 
+            {
+                Console.Write(array[i, j, k] + "(" + i + "," + j + "," + k + ") ");
+            }   
+            Console.WriteLine();
+        }
+    }
+}
+
+void GetArray(int[,,] array)
+{
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+        for (int k = 0; k < 2; k++) 
+        {
+            array[i, j, k] = new Random().Next(0, 99);
+        }
+    }
+  }
+}
+
+
+GetArray(array);
+Result(array);
